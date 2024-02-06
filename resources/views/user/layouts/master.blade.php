2ea5a870cfc706422b52e683bf1c5e0fb873ef2a
naffsvn <naffsvn@protonmail.com>
2024-02-06 22:02:05 +0700

    fix: add preloader for user view

--- resources/views/user/layouts/master.blade.php
+++ resources/views/user/layouts/master.blade.php
@@ -9,9 +9,16 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     <title>Napuniverse - Blog</title>
 </head>
 <body>
+    <div id="preloader">
+        <div id="loader" class="h-screen flex justify-center items-center"><span class="loading loading-dots loading-md"></span></div>
+    </div>
+
     <!-- Navigation -->
     @include('user.layouts.navigation')
 
@@ -23,7 +30,12 @@
     <!-- Footer -->
     @include('user.layouts.footer')
 
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script>
+        window.addEventListener('load', function() {
+            const preloader = document.getElementById('preloader');
+            preloader.style.display = 'none';
+        });
+    </script>
+
 </body>
 </html>
